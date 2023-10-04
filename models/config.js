5788601c321b9559d10f2f3725a82c23b5b90799
monus97 <monus3871@gmail.com>
2023-10-04 13:35:15 +0530

    working config file

--- models/config.js
+++ models/config.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.URL,{useNewUrlParser: true})
 const connection = mongoose.connection
 connection.once('open',(req,res)=>{
-    console.log(`mongodb connection set successfull ${process.env.URL}`)
+    console.log(`mongodb connection set successfull`)
 })
